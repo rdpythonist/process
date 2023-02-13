1f907b102c069444a34ff6ab198d1366e08393a3
rdpythonisst <rahulpanchal7698@gmail.com>
2023-02-13 22:29:55 +0530

    newcommit

--- process.py
+++ process.py
@@ -1,5 +1,5 @@
 import subprocess,sys,json
-# p=subprocess.run(["powershell.exe","D:\\process\\hello.ps1"],capture_output=True,text=True)
+# p=subprocess.run(["powershell.exe","D:\\process\\hello.ps1"],capture_output=True,text=True) #for singlw
 p=subprocess.Popen(["powershell.exe","D:\\process\\hello.ps1"],stdout=subprocess.PIPE,text=True)
 out,err=p.communicate()
 print(json.dumps(out))
