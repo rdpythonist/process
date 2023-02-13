f28d566b696bfcc1a5be15777ac1ce3309f56628
rdpythonisst <rahulpanchal7698@gmail.com>
2023-02-13 23:32:57 +0530

    Chaneg in code

--- process.py
+++ process.py
@@ -1,5 +1,15 @@
 import subprocess,sys,json
-# p=subprocess.run(["powershell.exe","D:\\process\\hello.ps1"],capture_output=True,text=True) #for singlw
-p=subprocess.Popen(["powershell.exe","D:\\process\\hello.ps1"],stdout=subprocess.PIPE,text=True)
-out,err=p.communicate()
-print(json.dumps(out))
+# # p=subprocess.run(["powershell.exe","D:\\process\\hello.ps1"],capture_output=True,text=True) #for singlw
+# p=subprocess.Popen(["powershell.exe","D:\\process\\hello.ps1"],stdout=subprocess.PIPE,text=True)
+# out,err=p.communicate()
+import time
+from datetime import datetime
+for i in range(10):
+    p=subprocess.Popen(["powershell.exe","D:\\process\\hello.ps1"],stdout=subprocess.PIPE,text=True)
+    out,err=p.communicate()
+    current_datetime=str(datetime.now()).replace(':','')
+    file_name=str(current_datetime)[:20]+'.txt'
+    file=open(file_name,'w')
+    file.write(str(out))
+    file.close()
+    time.sleep(3)
